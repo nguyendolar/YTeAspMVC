754fd2b118000d012df138dc2ff18e9354bdb805
nguyendolar <nguyencaonguyencmu@gmail.com>
2022-10-30 22:18:56 +0700

    first commit

---
+++ YTeAspMVC/Controllers/AuthencationController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YTeAspMVC.Controllers
+{
+    public class AuthencationController : Controller
+    {
+        // GET: Authencation
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Singup()
+        {
+            return View();
+        }
+    }
+}
---
+++ YTeAspMVC/Models/Number.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace YTeAspMVC.Models
+{
+    public class Number
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdNumber { get; set; }
+
+
+        [StringLength(255)]
+        [Required]
+        public string Image { get; set; }
+
+        public int NumberInt { get; set; }
+
+        public string Day { get; set;  }
+
+        public int IdUser { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
